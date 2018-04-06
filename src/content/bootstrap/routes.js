c10c767aefbcbaf36130509292a6b3d4322d8a47
Thomas Pickard <tom@sliips.com>
2018-04-06 11:38:16 +0100

    tmp

--- src/content/bootstrap/routes.js
+++ src/content/bootstrap/routes.js
@@ -5,7 +5,7 @@ import LegalPage from '../../content/containers/Pages/Legal';
 import MeetMePage from '../../content/containers/Pages/MeetMe';
 import DataPage from '../../content/containers/Pages/Data';
 import BenefitsPage from '../../content/containers/Pages/Benefits';
-
+import NewTestPage from '../../content/containers/Pages/NewTest';
 
 export default [
   {
@@ -37,5 +37,10 @@ export default [
     path: '/legal',
     exact: true,
     component: LegalPage,
+  },
+  {
+    path: '/new-test',
+    exact: true,
+    component: NewTestPage,
   },
 ];
